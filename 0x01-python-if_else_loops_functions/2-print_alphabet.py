18d570d7c9377cd179c0c9a9cc2303fce4a4e9a1
mugambi12 <silasmungiria16@gmail.com>
2023-05-10 04:36:16 +0300

    2. I sometimes suffer from insomnia

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-"""
-Print the alphabet in lowercase, not followed by a new line.
-"""
-
-# loop through the ASCII values for lowercase letters (97-122)
+"""Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-    # use chr() to convert each value to its corresponding char and print it
     print("{}".format(chr(letter)), end="")
-
-# print a new line character at the end
-print()

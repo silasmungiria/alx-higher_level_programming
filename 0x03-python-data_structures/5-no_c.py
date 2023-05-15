784b33e649a47e7e991760d713d38eabbf1fbc49
mugambi12 <silasmungiria16@gmail.com>
2023-05-15 22:44:46 +0300

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    copy_str = [x for x in my_string if x.lower() != 'c']
+    return "".join(copy_str)

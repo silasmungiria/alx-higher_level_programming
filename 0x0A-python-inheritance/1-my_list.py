a8f7be0132a52c1f0c12c475a3d044b2a47d5e99
SILAS MUGAMBI <114164521+Mugambi12@users.noreply.github.com>
2023-06-15 01:26:43 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,8 +3,13 @@
 contains the MyList class
 """
 
-class MyList(list):
-    def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
 
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))

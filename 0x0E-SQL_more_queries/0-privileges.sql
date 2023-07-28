a682057b3f8ee722c5a442b121da00dfe32f98db
mugambi12 <silasmungiria16@gmail.com>
2023-07-28 11:31:54 +0300

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
+mysql -hlocalhost -uroot -p
+
+-- Enter your password when prompted
+
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

4d47020fb841ebbdbd595137c2675a03c1a6627b
mugambi12 <silasmungiria16@gmail.com>
2023-08-14 22:16:36 +0300

    Update JS Warm Up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,13 +3,11 @@
 // This line specifies the path to the interpreter for the script.
 
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  // This block of code checks if the argument provided in the command line
-  // is not a number or if no argument is provided.
-  // If either condition is true, it outputs 'Not a number' to the console.
+  console.log('Not a number');
+  // If the command-line argument is not a number or is undefined,
+  // print 'Not a number'.
 } else {
-  // If the conditions in the "if" statement are not met, this block
-  // of code executes.
   console.log('My number:', parseInt(process.argv[2]));
-  // This line outputs the message 'My number:' followed by the parsed integer
-  // value of the command-line argument.
+  // If the argument is a valid number, print 'My number:' followed by,
+  // the parsed integer value.
 }

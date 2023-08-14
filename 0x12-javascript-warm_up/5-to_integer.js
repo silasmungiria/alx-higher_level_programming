10b5ca1a52a30c7510c6ff9e2d8e2b6f4f843494
mugambi12 <silasmungiria16@gmail.com>
2023-08-14 22:10:38 +0300

    Update JS Warm Up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,13 +3,13 @@
 // This line specifies the path to the interpreter for the script.
 
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  // This block of code checks if the argument provided in the command line,
+  // This block of code checks if the argument provided in the command line
   // is not a number or if no argument is provided.
   // If either condition is true, it outputs 'Not a number' to the console.
 } else {
-  // If the conditions in the "if" statement are not met, this block,
+  // If the conditions in the "if" statement are not met, this block
   // of code executes.
   console.log('My number:', parseInt(process.argv[2]));
-  // This line outputs the message 'My number:' followed by the parsed integer,
-  // value of the command - line argument.
+  // This line outputs the message 'My number:' followed by the parsed integer
+  // value of the command-line argument.
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 // This line specifies the path to the interpreter for the script.
 
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  // This block of code checks if the command-line argument is,
+  // This block of code checks if the command-line argument is
   // undefined or not a number.
   // If either condition is true, it outputs 'Missing size' to the console.
 } else {
@@ -11,17 +11,17 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   // this block of code executes.
 
   const x = Number(process.argv[2]);
-  // This line converts the command-line argument to a number and,
+  // This line converts the command-line argument to a number and
   // assigns it to the variable "x".
 
   let i = 0;
   // This line initializes a variable "i" to 0, which will be used as a counter.
 
   while (i < x) {
-    // This loop will continue as long as the value of "i" is less than,
+    // This loop will continue as long as the value of "i" is less than
     // the value of "x".
     console.log('X'.repeat(x));
-    // This line outputs a string consisting of 'X' repeated "x" times to,
+    // This line outputs a string consisting of 'X' repeated "x" times to
     // the console.
     i++;
     // This line increments the value of "i" by 1 in each iteration.

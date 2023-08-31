7b2839e0514abf05bbd9969454395c4e30dd6007
mugambi12 <silasmungiria16@gmail.com>
2023-08-31 20:19:51 +0300

    Python - Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# This script takes a URL as an argument, sends a request to that URL,
+# and displays the size of the body of the response in bytes.
+
+# Send a GET request to the provided URL and pipe the response body
+# to "wc" command to count the bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+

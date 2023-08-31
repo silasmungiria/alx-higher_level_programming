aafc6f7a1d8b06709639efea2a4e49d64ece86bc
mugambi12 <silasmungiria16@gmail.com>
2023-08-31 20:24:08 +0300

    Python - Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL,
-# and displays the size of the body of the response in bytes.
-
-# Send a GET request to the provided URL and pipe the response body
-# to "wc" command to count the bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
-
+# Make request and display response
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2

f60bae210f0c962b357e083cf12f6706e4bf4b7f
mugambi12 <silasmungiria16@gmail.com>
2023-09-27 20:30:23 +0300

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request(url, { encoding: 'utf-8' }, (error, response, body) => {
   if (error) {
     console.error(error);
     return;

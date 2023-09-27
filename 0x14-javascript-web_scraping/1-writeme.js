6a352ce34fbc6820a386b28ab22a060b80dab57e
mugambi12 <silasmungiria16@gmail.com>
2023-09-27 19:19:03 +0300

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,16 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('The file has been saved!');
+fs.writeFile(filePath, content, (err) => {
+//  if (err) {
+//    console.error(err);
+//    return;
+//  }
+//  console.log('The file has been saved!');
+//});
+
+//#!/usr/bin/node
+//const fs = require('fs');
+//fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
